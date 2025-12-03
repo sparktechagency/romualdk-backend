90f87db04ba38a5935e26713e9bcda703d19d738
Moshfiqur Rahman <studentemam@gmail.com>
2025-12-03 15:26:39 +0600

    feat: add media interface

---
+++ src/app/modules/media/media.interface.ts
@@ -0,0 +1,12 @@
+export enum MEDIA_TYPE {
+  BANNER = "BANNER",
+  FEED = "FEED"
+}
+
+export interface IMedia extends Document {
+  name: string;
+  image: string;
+  status: boolean;
+  type: MEDIA_TYPE;
+  description?: string;
+}
