79eeaa6cc42f91690f236dcc692420b51eaf79b9
Moshfiqur Rahman <studentemam@gmail.com>
2025-12-03 16:56:59 +0600

    feat: add support schema model

---
+++ src/app/modules/support/support.model.ts
@@ -0,0 +1,33 @@
+import { model, Schema } from "mongoose";
+import { TSupport } from "./support.interface";
+
+const supportSchema = new Schema<TSupport>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    subject: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Support = model("Support", supportSchema);
